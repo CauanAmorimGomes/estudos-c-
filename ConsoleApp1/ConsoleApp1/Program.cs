19f79d2c669dc5ec775a4e15b0365e08c8e10084
Cauan Amorim Gomes <162215277+CauanAmorimGomes@users.noreply.github.com>
2025-02-20 18:07:55 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 3 + 2;
+            float b = 3 - 2;
+            float c = 3 * 2;
+            float d = 3 / 2f; // Adicionando 'f' para garantir que seja um float
+            float e = 3 % 2;
+
+            System.Console.WriteLine($"a: {a}, b: {b}, c: {c}, d: {d}, e: {e}");
+            System.Console.ReadLine();
+        }
+    }
+}
